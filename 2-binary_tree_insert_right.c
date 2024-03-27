d41b28a9f7341d8c70c74813ac7a33bf08c728e1
LovethLaw <talktoawi@gmail.com>
2024-03-27 07:31:21 -0700

    fixeed

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	node = binary_tree_node(parent, value);
+	nodes = binary_tree_node(parent, value);
 
 	if (nodes == NULL)
 	{

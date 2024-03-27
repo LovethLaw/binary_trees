c8e1b4d975539322c618e0bd517ee2022b5167a3
LovethLaw <talktoawi@gmail.com>
2024-03-27 07:27:53 -0700

    data

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,16 +8,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *nodes;
+	binary_tree_t *node;
 
-	nodes = malloc(sizeof(binary_tree_t));
-	if (nodes == NULL)
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
 	{
 		return (NULL);
 	}
-	nodes->n = value;
-	nodes->parent = parent;
-	nodes->left = NULL;
-	nodes->right = NULL;
-	return (nodes);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,32 +1,31 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - node as the left-child of another node.
- * @parent: insert the left-child in.
- * @value: store in the new node.
+ * binary_tree_insert_left -left-child of another node.
+ * @parent: insert left-child in.
+ * @value: store in new node.
  *
- * Return: NULL on failure or if parent is NULL.
+ * Return: failure or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *nodes;
 
 	if (parent == NULL)
 		return (NULL);
 
-	node = binary_tree_node(parent, value);
+	nodes = binary_tree_node(parent, value);
 
-	if (node == NULL)
+	if (nodes == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
 	{
-		node->left = parent->left;
-		parent->left->parent = node;
+		nodes->left = parent->left;
+		parent->left->parent = nodes;
 	}
 
-	parent->left = node;
+	parent->left = nodes;
 
-	return (node);
+	return (nodes);
 }
-
 
